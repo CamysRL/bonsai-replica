016044a816d245cd77fd4581fac976ec111db5a5
CamysRL <camilly.rodrigues9999@gmail.com>
2026-04-14 16:35:16 -0300

    header completed

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,10 @@ function App() {
     <>
       <Header />
       <p>Hello, World!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat sint facilis voluptatibus. Autem ullam magnam dolorum obcaecati temporibus, quia, delectus quis natus pariatur voluptatum dolorem culpa fuga repellat provident saepe!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat sint facilis voluptatibus. Autem ullam magnam dolorum obcaecati temporibus, quia, delectus quis natus pariatur voluptatum dolorem culpa fuga repellat provident saepe!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat sint facilis voluptatibus. Autem ullam magnam dolorum obcaecati temporibus, quia, delectus quis natus pariatur voluptatum dolorem culpa fuga repellat provident saepe!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat sint facilis voluptatibus. Autem ullam magnam dolorum obcaecati temporibus, quia, delectus quis natus pariatur voluptatum dolorem culpa fuga repellat provident saepe!</p>
     </>
   );
 }
