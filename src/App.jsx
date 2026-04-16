9b63cd23fa87fb8422218f4ed1231ac0cbbb46cc
CamysRL <camilly.rodrigues9999@gmail.com>
2026-04-16 11:13:59 -0300

    fix the React Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Home, About, Pricing } from "./pages";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
       <Footer />
     </>
   );
